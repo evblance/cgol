7dad96ea652a8ba06081f0df51dda4a35d260b09
evblance <evblance@users.noreply.github.com>
2019-02-06 08:53:28 +1100

    Removes a redundant auto-import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import Gameboard from './components/Gameboard';
 import './App.css';
 import styled from 'styled-components'
